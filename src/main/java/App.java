dd4f3c83c924fde9052e9f84ffa5fca602437c03
BelalovKamil <belkamil@gmail.com>
2021-08-30 14:23:10 +0500

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloworld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloworld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Сравнение beans с дефолтным scope: " + (helloworld==helloworld1));
+        ApplicationContext applicationContext2 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat cat = (Cat)applicationContext2.getBean("cat");
+        Cat cat1 = (Cat) applicationContext2.getBean("cat");
+        System.out.println("Сравнение beans с prototype scope: " + (cat==cat1));
     }
 }
